011c47fde9fcea4f46ef4c3d58c16499fc753a0a
mounica78 <54437485+mounica78@users.noreply.github.com>
2019-09-17 10:57:59 +0530

    Add files via upload

---
+++ source1.js
@@ -0,0 +1,10 @@
+function add(a) {
+    if (a)
+        return a;
+function add(a, b) {
+    if (a && b)
+        return a + b;
+    return 0;
+}
+
+exports.add = add
---
+++ test.js
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+'use strict'
+
+var source = require('./source')
+var should = require('should')
+
+describe('first set', function () {
+    
+    it('first test with valid arguments', function (done) {
+        var a = 1,
+            b = 2,
+            expected = 3;
+        var result = source.add(a, b)
+        result.should.equal(expected)
+        done()
+    })
+    it('first test with valid big arguments', function (done) {
+        var a = 100000,
+            b = 200000,
+            expected = 300000;
+        var result = source.add(a, b)
+        result.should.equal(expected)
+        done()
+    })
+})
+=======
+'use strict'
+
+var source = require('./source')
+var should = require('should')
+
+describe('first set', function () {
+    
+    it('first test with valid arguments', function (done) {
+        var a = 1,
+            b = 2,
+            expected = 3;
+        var result = source.add(a, b)
+        result.should.equal(expected)
+        done()
+    })
+    it('first test with valid big arguments', function (done) {
+        var a = 100000,
+            b = 200000,
+            expected = 300000;
+        var result = source.add(a, b)
+        result.should.equal(expected)
+        done()
+    })
+})
+>>>>>>> 33a0e936c7bd2c62b54e98a7b311766313d488b4
