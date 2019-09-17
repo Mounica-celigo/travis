60aec1da96fa8bdd140ebcba586933f971823a4d
mounica78 <54437485+mounica78@users.noreply.github.com>
2019-09-17 12:10:33 +0530

    Update source1.js

--- source1.js
+++ source1.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 function add(a) {
     if (a)
         return a;
-=======
 function add(a, b) {
     if (a && b)
         return a + b;
->>>>>>> 33a0e936c7bd2c62b54e98a7b311766313d488b4
     return 0;
 }
-
 exports.add = add
